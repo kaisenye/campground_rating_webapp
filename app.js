6bcae979ff16eed6c4d1b3f7ec0a609cfd8a1311
Kaisen Ye <75810017+kaisenye@users.noreply.github.com>
2022-10-21 00:05:27 -0400

    app design update

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ db.once("open", () => {
 
 const app = express();
 
+// link to the 'views' folder
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
@@ -29,37 +30,40 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
-
+// render the website
 app.get('/', (req, res) => {
     res.render('home')
 });
+
+// home page
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds })
 });
+
+// add new page
 app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 })
 
-// add new campgrounds
 app.post('/campgrounds', async (req, res) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
 })
 
+// show page
 app.get('/campgrounds/:id', async (req, res,) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show', { campground });
 });
 
-// get existigng campgrounds
+// update page
 app.get('/campgrounds/:id/edit', async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/edit', { campground });
 })
 
-// update existing campgrounds
 app.put('/campgrounds/:id', async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
